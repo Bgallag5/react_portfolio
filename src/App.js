3e495a004f864faf25ade030293bef4a607a7ef9
bengallagher <bgallag5@gmail.com>
2021-10-02 09:15:40 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function App() {
+  return (
+    null
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+// Notes
+//Sections: About me, Portfolio, Contact, Resume 
+//single page app- components are reloaded individually 
+//Navbar: nav links, active is highlighted
+//About Me: default section loaded; photo and short bio
+//Portfolio: 6 projects previewed, link to the deployed projects 
+//Contact: name, email, (info) - and contact form? ; error message when the user leaves an...`  
+// input feild blank ; email is validated, else an error message
+//Resume: downloadable pdf - short "preview" snippit of resume ie. list technologies, skills 
+//Footer: email, github, LinkedIn
+
+
+
