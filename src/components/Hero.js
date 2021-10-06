5e34f892ddc718abad5bc8c9849a772d8bb75575
bengallagher <bgallag5@gmail.com>
2021-10-06 12:18:20 -0400

    restyled about me

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,21 +2,16 @@ import React from 'react'
 
 export default function Hero() {
 
-    const image = require('../assets/images/headshot.jpeg').default;
+    const image = require('../assets/images/AvatarMaker.svg').default;
 
 
     return (
         <div className='hero-container'>
-            <p className="hero-text-top">Hello! My name is Ben and I make websites.</p>
-            <br/>
 
             <div className='about-section'>
             <img src = {image} alt = 'Ben' id="about-picture"/>
-            <p>My name is Ben Gallagher and I like to make websites pls hire me RIGHT NOW!!!!
-            This is the second p tag that will appear in my about me section. This will probably be somewhat
-            long, as I will be describing all I have done over the last few months during this program.
-            </p>
-        </div>
+            <p className="hero-text-top">Hello! My name is Ben and I like to code.</p>
+            </div>
         
         </div>
     )
